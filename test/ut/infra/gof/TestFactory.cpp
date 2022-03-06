687adef941a5af44015a6e968f024649f030bdeb
lichengyu <lichengyu2345@126.com>
2022-03-07 07:02:12 +0800

    add test case for Factory

--- test/ut/infra/gof/TestFactory.cpp
+++ test/ut/infra/gof/TestFactory.cpp
@@ -4,18 +4,42 @@
 USING_HICCUP
 
 namespace {
-    struct Foo {
+    uint32_t resourceOccupied = 0;
 
+    struct Foo {
+        Foo(int v) : x{v} {
+            resourceOccupied++;
+        }
+
+        int getValue() const {
+            return x;
+        }
+
+        ~Foo() {
+            resourceOccupied--;
+        }
+
+    private:
+        int x{0};
     };
 
     DEF_FACTORY_OF(Foo) {
-        
+
     };
 }
 
 TEST_CASE("Test Factory") {
 
     SECTION("should create entity") {
-        auto f1 = FACTORY_OF(Foo).create();
+        auto f1 = FACTORY_OF(Foo).create(1);
+        REQUIRE(f1->getValue() == 1);
+
+        SECTION("should create more entities") {
+            auto f2 = FACTORY_OF(Foo).create(2);
+            REQUIRE(f2->getValue() == 2);
+            REQUIRE(resourceOccupied == 2);
+        }
+        REQUIRE(resourceOccupied == 1);
     }
+    REQUIRE(resourceOccupied == 0);
 }
